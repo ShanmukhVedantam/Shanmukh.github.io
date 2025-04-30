4b1707dd22d7cfd4bf30cfce0b6bef80724fe734
Shanmukh Vedantam <shanmukh.ved@gmail.com>
2025-04-30 08:53:17 -0700

    Medium blog

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -219,7 +219,7 @@ const CONFIG = {
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'ShanmukhVedantam', // to hide blog section, keep it empty
+    username: 'shanmukh.ved', // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
