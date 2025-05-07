bf3ea3d38f8c4c21c91df2c942418986b3ec17f7
Shanmukh Vedantam <shanmukh.ved@gmail.com>
2025-05-07 14:38:10 -0700

    test grid

--- src/components/gitprofile.tsx
+++ src/components/gitprofile.tsx
@@ -239,12 +239,16 @@ const GitProfile = ({ config }: { config: Config }) => {
                     />
                   </div>
                 </div>
-                <div className="lg:col-span-1 col-span-1">
+                <div className="lg:col-span-2 col-span-1">
                   <div className="grid grid-cols-1 gap-6">
                     {sanitizedConfig.aboutMe && (
                       <AboutMe description={sanitizedConfig.aboutMe} 
                       loading={loading} />
                     )}
+                  </div>
+                </div>    
+                <div className="lg:col-span-2 col-span-1">
+                  <div className="grid grid-cols-1 gap-6">
                     {sanitizedConfig.experiences.length !== 0 && (
                       <ExperienceCard
                         loading={loading}
